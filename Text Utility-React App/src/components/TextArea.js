5ecf7cdfb56ff1712e3a5531fb2df6b6c06ba170
RaoAzlan7 <156425281+RaoAzlan7@users.noreply.github.com>
2024-03-15 09:47:10 +0000

    Update TextArea.js

--- Text Utility-React App/src/components/TextArea.js
+++ Text Utility-React App/src/components/TextArea.js
@@ -14,7 +14,13 @@ export default function TextArea(props){
         let newText = text.toLowerCase();
         setText(newText);
     }
-    
+    const handleCopy=()=>{
+        let txt = document.getElementById('textBox');
+        txt.select();
+        navigator.clipboard.writeText(txt.value);
+        props.showAlert("Copied to Clipboard","success");
+        
+    }
     const handleClearClick=()=>
     {
         setText("");
@@ -35,14 +41,15 @@ export default function TextArea(props){
           <textarea style={{resize: "none",
           backgroundColor:props.mode==='light'?'white':'#2c6cc9',
           border:props.mode==='light'?'':'2px solid black',
-          color:props.mode==='light'?'black':'cyan'} } className="form-control" onChange={handleChange} value={text} rows="8"></textarea>
+          color:props.mode==='light'?'black':'cyan'} } className="form-control" onChange={handleChange} id="textBox" value={text} rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleHiClick}>Convert to UpperCase</button>
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
         <br/>
         <button className="btn btn-primary my-1" onClick={handleReverseClick}>ɘƨɿɘvɘЯ</button>
-        
+        <button className="btn btn-primary my-1 mx-1" onClick={handleCopy}>Copy Text</button>
+
         <h2>Your text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p> {Math.round((1/300)*text.split(" ").length*1000)/1000} <b>minutes to read.</b></p>
